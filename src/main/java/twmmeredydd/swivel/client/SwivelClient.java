0207ff878ebf9e9b475d051eace2bea84ea4d462
TwmMeredydd <49553663+TwmMeredydd@users.noreply.github.com>
2024-08-28 15:14:37 +0100

    Initial template commit

---
+++ src/main/java/twmmeredydd/swivel/client/SwivelClient.java
@@ -0,0 +1,15 @@
+package twmmeredydd.swivel.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SwivelClient implements ClientModInitializer {
+	public static final String MOD_ID = "swivel";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
